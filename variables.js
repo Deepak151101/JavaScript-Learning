fa3cf53580c61980b43e06bf8c478eeae9df97fb
Deepak Javalgi <74924437+Deepak151101@users.noreply.github.com>
2025-01-18 20:10:38 +0530

    Delete variables.js

--- variables.js
+++
@@ -1,8 +0,0 @@
-const accountID = 123;
-var accEmail = "test@gmail.com";
-let accPass = "testPass";
-accountCity = "Pune"; // Not a good practice
-let accountState;
-
-console.log(accountID);
-console.table([accountID ,accEmail, accPass, accountCity, accountState]);
