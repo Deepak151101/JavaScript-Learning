cccbff9a135b74172be1a6434eaec84433e0a947
imdeepakjavalgi <imdeepakjavalgi@gmail.com>
2025-01-18 17:25:05 +0000

    Variables

---
+++ Basics/Variables.js
@@ -0,0 +1,8 @@
+const accountID = 123;
+var accEmail = "test@gmail.com";
+let accPass = "testPass";
+accountCity = "Pune"; // Not a good practice
+let accountState;
+
+console.log(accountID);
+console.table([accountID ,accEmail, accPass, accountCity, accountState]);
