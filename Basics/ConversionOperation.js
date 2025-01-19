b965df9bcbe41ed4a1eda5dcde3af26ae0133f5e
imdeepakjavalgi <imdeepakjavalgi@gmail.com>
2025-01-19 12:35:46 +0000

    Operators basic operations

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,50 @@
+let value = 3;
+let NegValue = -value;
+
+// console.log(NegValue);
+
+// Basic Arithmetic Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Deepak";
+let str2 = "Javalgi";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log(1 + "2"); // 12
+console.log("1"+ 2); // 12
+console.log("1"+ 2 + 2); // 122
+console.log(1+ 2 + "2"); // 32
+console.log("---------------------------------------------------");
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+console.log("---------------------------------------------------");
+let num1 = num2 = num3 = 17;
+console.table([num1, num2, num3]); // 17 17 17
+
+console.log("---------------------------------------------------");
+
+let gameCounter1 = 100;
+++gameCounter1;
+console.log(gameCounter1); // 101
+
+let gameCounter2 = 200;
+gameCounter2++;
+console.log(gameCounter2); // 201
+
+let gameCounter3 = 300;
+--gameCounter3;
+console.log(gameCounter3); // 299
+
+let gameCounter4 = 400;
+gameCounter4--;
+console.log(gameCounter4); // 399
