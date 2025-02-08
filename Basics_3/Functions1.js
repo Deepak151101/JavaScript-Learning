4755869c6b255067d2fcd84637b59ea0655f725b
imdeepakjavalgi <imdeepakjavalgi@gmail.com>
2025-02-08 15:06:58 +0000

    Functions with Objects & Arrays

--- Basics_3/Functions1.js
+++ Basics_3/Functions1.js
@@ -37,3 +37,42 @@ function loginUserMsg(username = "Sam") {
 }
 console.log(loginUserMsg("Deepak"));    // Deepak just logged in
 console.log(loginUserMsg());    // Sam just logged in
+
+// Example - Function for a shopping cart example where we do not know how many parameters (items) user will buy
+
+// Here we use rest operator. It is denoted by (...)
+
+function calcuateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+console.log(calcuateCartPrice(100, 200, 300, 400, 500));        // [ 300, 400, 500 ] -> 100 is taken in val1 & 200 is taken in val2
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// Passing object in function
+handleObject(user);     // Username is hitesh and price is 199
+// or
+handleObject({          // // Username is Deepak and price is 59
+    username: "Deepak",
+    price: 59
+});     
+
+// Passing array in function
+// Write a function that accepts a array and returns 2nd element
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));     // 400
+console.log(returnSecondValue(['a', 'b', 'c', 'z']));       // b
