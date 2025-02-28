ba9a7b1c74a688a938bee7afdfac126c41139962
imdeepakjavalgi <imdeepakjavalgi@gmail.com>
2025-02-28 12:45:27 +0000

    Project 3 - Digital Clock

--- Projects/Digital Clock/script.js
+++ Projects/Digital Clock/script.js
@@ -3,7 +3,8 @@ const clock = document.getElementById('clock');
 
 setInterval(function () {
     let date = new Date();
-    clock.innerHTML = date.toLocaleString();
+    clock.innerHTML = date.toLocaleString();        // 28/2/2025, 6:18:22 pm
+    // clock.innerHTML = date.toLocaleTimeString();        // 6:18:39 pm
 }, 1000);
 
 // In setInterval, we specify time in miliseconds. Any function written in it will run every 1000 ms or 1 seconds
